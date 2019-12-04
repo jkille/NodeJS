3c6a8aa5e4ee8f8e0c93554e5593cd7180222d13
Khun Htetz Naing <khunhtetznaing@gmail.com>
2019-12-04 14:34:50 +0630

    add update font

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const Font = require("./models/FontSchema"),
 io = require('socket.io').listen(server);
 app.get('/', (req, res) => {
 
-      font_db.then(db => {
-            Font.find({}).then(font => {
-                res.send(font);
-            });
+    font_db.then(db => {
+        Font.find({}).then(font => {
+            res.send(font);
         });
+    });
 
     //Read
     // connect.then(db => {
@@ -43,6 +43,21 @@ io.on('connection', (socket) => {
         });
     });
 
+    socket.on('update_font', (id, mTitle, mUrl, mNew, mThumbnail, mSize, mAuthor, mDesigner, mLanguage) => {
+        var font = { title: mTitle, url: mUrl, is_new: mNew, size: mSize, thumbnail: mThumbnail, author: mAuthor, designer: mDesigner, count: "1", language: mLanguage };
+        for (let key in font) {
+            if (font[key] == 1) {
+                delete font[key];
+            }
+        }
+
+        Font.findOneAndUpdate(id, font, { new: true }, function (err, doc) {
+            if (err) return res.send(500, { error: err });
+            return console.log('Updated: '+mTitle);
+        });
+
+        
+    });
 
     socket.on('add_font', (mTitle, mUrl, mNew, mThumbnail, mSize, mAuthor, mDesigner, mLanguage) => {
         let font = new Font({ title: mTitle, url: mUrl, is_new: mNew, size: mSize, thumbnail: mThumbnail, author: mAuthor, designer: mDesigner, count: "1", language: mLanguage });
