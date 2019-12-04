737aee8906865f03bede855d5fb7d44721b8bf33
Khun Htetz Naing <khunhtetznaing@gmail.com>
2019-12-04 12:33:44 +0630

    first

--- server.js
+++ server.js
@@ -1,55 +1,68 @@
-const  Chat  = require("./models/FontSchema"),
-connect  = require("./db"),
-express = require('express'),
-http = require('http'),
-app = express(),
-server = http.createServer(app),
+const Font = require("./models/FontSchema"),
+    connect = require("./db"),
+    express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app)
 io = require('socket.io').listen(server);
 app.get('/', (req, res) => {
+    res.send('Succesfully!');
+    //Read
+    // connect.then(db => {
+    //     Font.find({}).then(font => {
+    //         res.send(font);
+    //     });
+    // });
 
-res.send('Chat Server is running on port 3000')
+    // let font = { title: "NEW_TITLE2"};
+    // Font.findOneAndUpdate('5de7475760fe41009da1a747', font, { new: true }, function (err, doc) {
+    //     if (err) return res.send(500, { error: err });
+    //     return res.send('Succesfully saved.');
+    // });
 });
+
+
 io.on('connection', (socket) => {
 
-console.log('user connected')
-
-socket.on('join', function(userNickname) {
-
-        console.log(userNickname +" : has joined the chat "  );
-
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+    socket.on('open', function (userNickname) {
+        console.log(userNickname + " : has open the app ");
+        connect.then(db => {
+            Font.find({}).then(font => {
+                socket.broadcast.emit('useropentheapp', font);
+            });
+        });
     });
 
 
-socket.on('messagedetection', (senderNickname,messageContent) => {
-       
-       //log the message in console 
+    socket.on('messagedetection', (senderNickname, messageContent) => {
 
-       console.log(senderNickname+" :" +messageContent)
+        //log the message in console 
+
+        console.log(senderNickname + " :" + messageContent)
         //create a message object 
-       let  message = {"message":messageContent, "senderNickname":senderNickname}
-          // send the message to the client side  
-       io.emit('message', message );
-     
-      });
-      
-  
- socket.on('disconnect', function() {
-    console.log( ' user has left ')
-    socket.broadcast.emit("userdisconnect"," user has left ") 
+        let message = { "message": messageContent, "senderNickname": senderNickname }
+        // send the message to the client side  
+        io.emit('message', message);
+
+    });
+
+
+    socket.on('disconnect', function () {
+        console.log(' user has left ')
+        socket.broadcast.emit("userdisconnect", " user has left ")
+
+    });
+
+
 
 });
 
 
 
-});
 
 
+server.listen(3000, () => {
 
-
-
-server.listen(3000,()=>{
-
-console.log('Node app is running on port 3000');
+    console.log('Node app is running on port 3000');
 
 });
